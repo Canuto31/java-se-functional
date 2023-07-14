39b651896b8144b2be471b711649b18c2a7a9914
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-07-14 18:42:49 -0500

    More about the Streams

--- src/main/java/com/courses/_15_streams_intro/Streams.java
+++ src/main/java/com/courses/_15_streams_intro/Streams.java
@@ -1,7 +1,6 @@
 package com.courses._15_streams_intro;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Stream;
 
 import com.courses._06_reference_operator.NombresUtils;
@@ -27,5 +26,14 @@ public class Streams {
         Stream<String> justJavaCourses = emphasisCoursesStream.filter(course -> course.toLowerCase().contains("java"));
 
         justJavaCourses.forEach(System.out::println);
+
+        Stream<String> coursesStream2 = courseList.stream();
+
+        addOperator(coursesStream2.map(course -> course + "!!").filter(course -> course.contains("Java"))).forEach(System.out::println);
+        
     }
+
+    static <T> Stream<T> addOperator(Stream<T> stream) {
+            return stream.peek(data -> System.out.println("Data = " + data));
+        }
 }
