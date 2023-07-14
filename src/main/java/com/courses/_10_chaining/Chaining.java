5bad8d2da2cbd61094777c3e7eaf13b12129b43d
canuto31-admin <jcmt.personal.vg@gmail.com>
2023-07-14 07:50:20 -0500

    First steps with Chainers

---
+++ src/main/java/com/courses/_10_chaining/Chaining.java
@@ -0,0 +1,23 @@
+package com.courses._10_chaining;
+
+public class Chaining {
+    
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Hola").append("Mundo");
+
+        Chainer chainer = new Chainer();
+        chainer.sayHi().sayBye();
+    }
+
+    static class Chainer {
+        public Chainer sayHi() {
+            System.out.println("Hola");
+            return this;
+        }
+        public Chainer sayBye() {
+            System.out.println("Bye");
+            return this;
+        }
+    }
+}
